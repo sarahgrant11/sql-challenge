98266a6567ac4a39338dbe26751bd34bc80fa57a
Sarah Grant <sarahgrant11@gmail.com>
2020-10-05 14:34:53 -0400

    all files ready

---
+++ EmployeeSQL.sql
@@ -0,0 +1,10 @@
+--Create new table
+
+CREATE TABLE "Employees" (
+	"emp_no" INT NOT NULL,
+	"emp_title_id" VARCHAR NOT Null,
+	"birth_date" DATE NOT NULL,
+	"first_name" VARCHAR NOT NULL,
+	"last_name" VARCHAR NOT NULL,
+	"sex" VARCHAR NOT NULL,
+	"hire_date" DATE NOT NULL
